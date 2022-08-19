ec3a7e3848781d1c498358ab7caf4e667d319680
hunterde <51929871+hunterde@users.noreply.github.com>
2022-08-18 21:37:46 -0500

    Update NewEventForm
    Working on getting working event form

--- src/Components/NewEventForm.js
+++ src/Components/NewEventForm.js
@@ -1,18 +1,25 @@
 import './NewEventForm.css'
-import React from 'react'
+import React, { useState } from 'react'
+
 
 export default function NewEventForm() {
-  return (
+
+    const [title,setTitle]=useState('')
+    const [date,setDate]=useState('')
+   
+
+    return (
     <form className="new-event-form">
       <label>
         <span>Event Title:</span>
-      <input type="text" id="title"/>
+      <input type="text" onChange={(e)=> setTitle(e.target.value)}/>
       </label>
       <label>
         <span>Event Date:</span>
-        <input type="date"/>
+        <input type="date" onChange={(e)=> setDate(e.target.value)}/>
       </label>
       <button>Submit</button>
+      <p>title {title} date {date}</p>
     </form>
   )
 }
