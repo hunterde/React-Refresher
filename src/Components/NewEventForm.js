1b272cd8972691667ca8540fbb1264ae0dea945a
hunterde <51929871+hunterde@users.noreply.github.com>
2022-09-03 08:28:15 -0500

    Update NewEventForm.js
    Added Reset triggers on form

--- src/Components/NewEventForm.js
+++ src/Components/NewEventForm.js
@@ -7,19 +7,27 @@ export default function NewEventForm() {
     const [title,setTitle]=useState('')
     const [date,setDate]=useState('')
    
+    const resetForm=()=> {
+    setTitle('') 
+    setDate('')
+    }
 
     return (
     <form className="new-event-form">
       <label>
         <span>Event Title:</span>
-      <input type="text" onChange={(e)=> setTitle(e.target.value)}/>
+      <input type="text" onChange={(e)=> setTitle(e.target.value)}
+      value={title}/>
       </label>
       <label>
         <span>Event Date:</span>
-        <input type="date" onChange={(e)=> setDate(e.target.value)}/>
+        <input type="date" onChange={(e)=> setDate(e.target.value)}
+        value={date}/>
       </label>
       <button>Submit</button>
+      <button onClick={resetForm} >Reset</button>
       <p>title {title} date {date}</p>
+      <p onClick={resetForm} >Reset form</p>
     </form>
   )
 }
